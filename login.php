fb53ae3c89b72758227ad32e1f3a262b8bc207bf
CharlotteDupoux <charlottedupouxperso@hotmail.fr>
2015-10-05 17:45:02 +0200

    Fix#2

--- login.php
+++ login.php
@@ -14,13 +14,19 @@ require('model/functions.fn.php');
 if(isset($_POST['email']) && isset($_POST['password'])){
 	if(!empty($_POST['email']) && !empty($_POST['password'])){
 
+		if(UserConnection($db, $_POST['email'], $_POST['password'])){
+
+		}
+
+			header('Location: dashboard.php');
+
+
 		// TODO
 
 		// Force user connection to access dashboard
 		userConnection($db, 'git@initiation.com', 'password');
 		
-		header('Location: dashboard.php');
-
+		
 	}else{
 		$error = 'Champs requis !';
 	}
